93b3a316b92ce34c39b4fc7c8448340222c3b681
Jonathan Salcedo <jonatandavidsal@gmail.com>
2024-09-11 01:11:10 -0500

    Use of the controller in routes

--- controller/conversation.mjs
+++ controller/conversation.mjs
@@ -1,17 +1,17 @@
 import Chat from '../models/conversation.mjs'
 
-export const getData = (req, res) => {
+export const getData = async (req, res) => {
   const { chatId } = req.params
 
-  Chat.findById(chatId, (err, chat) => {
-    if (err) {
-      return res
-        .status(500)
-        .json({ message: 'Error al buscar el chat', error: err })
-    }
+  try {
+    const chat = await Chat.findById(chatId)
     if (!chat) {
       return res.status(404).json({ message: 'Chat no encontrado' })
     }
     return res.status(200).json(chat)
-  })
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: 'Error al buscar el chat', error: error.message })
+  }
 }
--- routes.mjs
+++ routes.mjs
@@ -1,4 +1,5 @@
 import express from 'express'
+import { getData } from './controller/conversation.mjs'
 import Chat from './models/conversation.mjs'
 
 export const router = express.Router()
@@ -15,14 +16,4 @@ router.post('/chat', async (req, res) => {
   }
 })
 
-router.get('/chat/:chatId', async (req, res) => {
-  const { chatId } = req.params
-
-  try {
-    const chat = await Chat.findById(chatId)
-    if (!chat) return res.status(404).json({ message: 'Chat not found' })
-    res.json(chat)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
-})
+router.get('/chat/:chatId', getData)
